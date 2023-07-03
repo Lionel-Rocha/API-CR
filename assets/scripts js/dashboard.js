2a8ff52c2bc52688736514e887d42c5c924f2595
Matheus Melo <melo.matheus13@outlook.com>
2023-07-03 20:19:36 -0300

    Dashboard atualizado

--- assets/scripts js/dashboard.js
+++ assets/scripts js/dashboard.js
@@ -1,6 +1,8 @@
 const data = JSON.parse(localStorage.getItem('data'));
 
 console.log('teste', data)
+areaName = data.areasch
+console.log(areaName)
 
 function setDashboard(){ 
     function cor(valor){  
@@ -10,7 +12,7 @@ function setDashboard(){
     }
 
     function setCRBar() {
-        const  crbar = data.crgeral.match(/\d+,\d{3}/)[0].replace(",", ".") * 10
+        const crbar = data.crgeral.match(/\d+,\d{3}/)[0].replace(",", ".") * 10
         const rgb = cor(crbar)
         
         document.getElementById("progress-spinner").style.background =`conic-gradient(${rgb}) ${crbar}%,rgb(242, 242, 242) ${crbar}%)`
@@ -21,4 +23,19 @@ function setDashboard(){
 
     setCRBar()
     // referencia: https://stackoverflow.com/questions/14222138/css-progress-circle
+
+    function setAreas() {
+        //const 
+        //const horasTotais = data.areasch[1].substring(0, )
+        let ulDisciplina = documento.getElementById("disciplinas-ul")
+        
+        for(i = 0; i < data.areasch.length; i++){
+            for(j = 0; j < 2; j++){
+                
+            }
+            console.log(areaName[i])
+        }
+    }
+
+    setAreas()
 } 
