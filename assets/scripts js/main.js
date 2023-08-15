d52bc0a22a94387ce3c219def5b2dbd94df28feb
Isabelle Araujo <isabelletecn@gmail.com>
2023-08-15 19:08:22 -0300

    responsividade header e about

---
+++ assets/scripts js/main.js
@@ -0,0 +1,5 @@
+document.querySelector("#menu-bar").addEventListener('click', () => {
+    let contentmenu = document.querySelector('#h-menu')
+    contentmenu.style.display == 'flex' ? contentmenu.style.display = 'none' : contentmenu.style.display = 'flex'
+    
+})
